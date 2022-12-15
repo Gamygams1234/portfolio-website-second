cda191020d6f1895bac8eab42eab40bbf8a594ea
Mateusz Koniuszaniec <koniuszaniec.mateusz@gmail.com>
2022-12-15 20:24:51 +0100

    button comp

---
+++ src/components/PrimaryButton.js
@@ -0,0 +1,15 @@
+import icon from '../images/icons/down-arrows.svg'
+import './PrimaryButton.css'
+
+
+
+function PrimaryButton(props) {
+    return <button>
+        <div class="icon">
+        <img src={icon} alt="down-arrows-icon" />
+        </div>
+        <span>{props.site}</span>
+    </button>
+}
+
+export default PrimaryButton;
