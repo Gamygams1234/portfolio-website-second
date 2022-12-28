55f79ef895043223df527110b9c319f61f3cdd21
Mateusz Koniuszaniec <koniuszaniec.mateusz@gmail.com>
2022-12-28 14:07:43 +0100

    mobile - header secion

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+
+import '../style/Header.css'
+import '../style/menu.css'
+
+const menu = document.querySelector('.hamburger');
+const nav_wrapper = document.querySelector('.nav_wrapper')
+
+const menuHandle = () => {
+    menu.classList.toggle('hamburger--active');
+    nav_wrapper.classList.toggle('show');
+}
+
+function Header() {
+    return ( 
+     <header>
+        <div className="logo_wrapper">
+            <a className ="logo" href="/"></a>
+        </div>
+        <nav>
+        <button onClick={menuHandle} class="hamburger" aria-label="Menu">
+            <span class="hamburger__container" tabindex="-1">
+              <span class="hamburger__bars"></span>
+            </span>
+          </button>
+          <div className="nav_wrapper">
+            <ul className="main_nav">
+                <li className="nav_link">HOME</li>
+                <li className="nav_link">PORTFOLIO</li>
+                <li className="nav_link">CONTACT ME</li>
+            </ul>
+          </div>
+        </nav>
+     </header>
+     );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import Header from './components/Header'
-import PrimaryButton from './components/PrimaryButton';
+import Hero from './components/Hero'
+// eslint-disable-next-line
+// import PrimaryButton from './components/PrimaryButton';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( 
-<>
-<Header />
-{/* <PrimaryButton site="About me"/> */}
+root.render(
+    <>
+    <Header/>
+    <Hero/>
     </>
-);
+    
+    // <h1>Hello</h1>
+) 
+;
